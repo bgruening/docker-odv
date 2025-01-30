b7a8c3a15a9e1bbbba8e363513946ae1a4123bd2
Marie Jossé <84919248+Marie59@users.noreply.github.com>
2025-01-30 15:55:29 +0100

    add environment varaibles startapp.sh

--- startapp.sh
+++ startapp.sh
@@ -18,6 +18,41 @@ ODVHOME=/app/odv
 #-----------------------------------------------------------------------
 # nothing to be changed below this line
 
+# test if ODV_ARGS exists
+
+echo checking ODV_ARGS
+
+# Check if the environment variable ODV_ARG1 is set and not empty
+if [ -n "${ODV_ARG1}" ]; then
+    set -- "$ODV_ARG1"
+    echo ODV_ARG1 is set to $ODV_ARG1
+fi
+
+# Check if the environment variable ODV_ARG2 is set and not empty
+if [ -n "${ODV_ARG2}" ]; then
+    set -- "$1" "$ODV_ARG2"
+    echo ODV_ARG2 is set to $ODV_ARG2
+fi
+
+# Check if the environment variable ODV_ARG3 is set and not empty
+if [ -n "${ODV_ARG3}" ]; then
+    set -- "$1" "$2" "$ODV_ARG3"
+    echo ODV_ARG3 is set to $ODV_ARG3
+fi
+
+# Check if the environment variable ODV_ARG4 is set and not empty
+if [ -n "${ODV_ARG4}" ]; then
+    set -- "$1" "$2" "$3" "$ODV_ARG4"
+    echo ODV_ARG4 is set to $ODV_ARG4
+fi
+
+# Check if the environment variable ODV_ARG5 is set and not empty
+if [ -n "${ODV_ARG5}" ]; then
+    set -- "$1" "$2" "$3" "$4" "$ODV_ARG5"
+    echo ODV_ARG5 is set to $ODV_ARG5
+fi
+
+
 # set LD_LIBRARY_PATH
 if [ -z LD_LIBRARY_PATH ]
 then
@@ -31,5 +66,10 @@ PATH=${PATH}:${ODVHOME}/bin_linux-amd64
 
 export LD_LIBRARY_PATH
 export PATH
+
+echo ODV home is       : ${ODVHOME}
+echo ODV executable is : ${ODVHOME}/bin_linux-amd64/odv
+echo ODV args are      : $1 $2 $3 $4 $5
+echo ODV command is    : ${ODVHOME}/bin_linux-amd64/odv $1 $2 $3 $4 $5
 
 exec ${ODVHOME}/bin_linux-amd64/odv $1 $2 $3 $4 $5
